2b2b4143817f5c8eef25f565b60f7f974e1b4e67
Nieves <143846293+Nieves433@users.noreply.github.com>
2023-11-28 09:16:46 -0300

    Update Arduino Code.ino

--- Código/Arduino/Arduino Code.ino
+++ Código/Arduino/Arduino Code.ino
@@ -2,19 +2,19 @@
 #include <Wire.h>
 #include <MultiFuncShield.h>
 #define MAX 5
-int Counter1 = 8, Down = 0, MaxRight = 0, Shoot = 1;
+int Counter1 = 5, Counter5 = 0, Down = 0, MaxRight = 0, Shoot = 1, Speed = 100;
 class Program{
   public:
    byte button;
-   int Counter2 = 1, Counter3 = 2, Time;
-   int MoveAlienShips( int MyTime ){ //Mueve las naves a der y a izq
+   int Counter2 = 1, Counter3 = 2, Counter4 = 3, Time;
+   int MoveAlienShips( int MyTime, int Speed ){ //Mueve las naves a der y a izq
     if ( MyTime == Counter2 ){ //MyTime = Counter2 para verificar que paso 1 seg
         switch( Down ){
           case 0:{
             switch( MaxRight ){
               case 0:{
                   Counter1--; //cuenta atras hasta chocar contra pared del diaplay
-                  Counter2++; //Cuenta 1 seg
+                  Counter2 += Speed; //Cuenta 1 seg
                   Serial.write( 'r' );
                   if ( Counter1 == 0 ){ 
                     MaxRight = 1;
@@ -23,9 +23,9 @@ class Program{
               }break;  
               case 1:{          
                   Counter1++; //cuenta atras hasta chocar contra esquida del diaplay
-                  Counter2++; //Cuenta 1 seg  
+                  Counter2 += Speed; //Cuenta 1 seg  
                   Serial.write( 'l' );
-                  if ( Counter1 == 8 ){ 
+                  if ( Counter1 == 5 ){ 
                     MaxRight = 0;
                     Down = 1;}
                   return( Counter2 );
@@ -33,19 +33,37 @@ class Program{
             }    
           }break;      
           case 1:{
-                Counter2++;
+                Counter2 += Speed;
                 Serial.write( 's' );
+                IncressDifficulty();
+                Counter5++;
                 Down = 0;
                 return( Counter2 );
           }break;       
    }}
    return(Counter2);}
-   int Shots( int MyTime1 ){
-      if( MyTime1 == Counter3 ){  //Siempre que pasen 3 seg
-        Counter3 += 2;
+   int Shoots( int MyTime1, int Speed ){
+      if( MyTime1 == Counter3 ){  //Siempre que pasen 2 seg
+        Counter3 += Speed*2;
         Serial.write( 'z' );
       }
       return( Counter3 );
+   }
+   int ShipsShoot( int MyTime1, int Speed ){
+      if( MyTime1 == Counter4 ){  //Siempre que pasen 3 seg
+        Counter4 += Speed;
+        int RandomNumber = int(random( 7 ));
+        Serial.write( 'c' );
+      }
+      return( Counter4 );
+   }
+   void IncressDifficulty(){
+      Speed -= 8;
+   }
+   void GameOver( int Counter5 ){ //Termina el juego tras enviar 8 veces "abajo"
+      if( Counter5 == 10 ){
+        Serial.write( 'G' );
+      }
    }
 };
 Program Game;
@@ -56,9 +74,11 @@ void setup() {
 }
 
 void loop() {
-  Game.Time = millis()/1000;
-  Game.Counter2 = Game.MoveAlienShips( Game.Time );
-  Game.Counter3 = Game.Shots( Game.Time );
+  Game.GameOver( Counter5 );
+  Game.Time = millis()/10;
+  Game.Counter2 = Game.MoveAlienShips( Game.Time, Speed );
+  Game.Counter3 = Game.Shoots( Game.Time, Speed );
+  Game.Counter4 = Game.ShipsShoot( Game.Time, Speed );
   Game.button = MFS.getButton();
       
   if ( Game.button ){
