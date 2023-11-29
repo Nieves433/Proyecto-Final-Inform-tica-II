01e888f1bcca6adcdccaa341e88d951f33398588
Nieves <143846293+Nieves433@users.noreply.github.com>
2023-11-29 12:47:22 -0300

    Update Arduino Code.ino

--- Código/Arduino/Arduino Code.ino
+++ Código/Arduino/Arduino Code.ino
@@ -1,14 +1,13 @@
-
-#include <TimerOne.h>
-#include <Wire.h>
 #include <MultiFuncShield.h>
 #define MAX 5
+FILE *fp;
 int Counter1 = 5, Counter5 = 0, Down = 0, MaxRight = 0, Shoot = 1, Speed = 100;
 class Program{
   public:
    byte button;
-   int Counter2 = 1, Counter3 = 2, Counter4 = 3, Time;
-   int MoveAlienShips( int MyTime, int Speed ){ //Mueve las naves a der y a izq
+   char Character;
+   int Counter2 = 1, Counter3 = 2, Counter4 = 2, Time;
+   int MoveAlienShips( int MyTime, int Speed ){ //Descripción: Cada 1 segundo envía un mensaje por puerto serial que indica nueva posición del atacante, sea hacia la izq, der, abajo y si es hacia abajo aumenta la dificultad. Parámetros: Tiempo transcurrido y deficultad. Retorna:Tiempo actualizado.
     if ( MyTime == Counter2 ){ //MyTime = Counter2 para verificar que paso 1 seg
         switch( Down ){
           case 0:{
@@ -18,7 +17,7 @@ class Program{
                   Counter2 += Speed; //Cuenta 1 seg
                   Serial.write( 'r' );
                   if ( Counter1 == 0 ){ 
-                    MaxRight = 1;
+                    MaxRight = 1; //Máxima posición horizontal alcanzada
                     Down = 1;}
                   return( Counter2 );
               }break;  
@@ -37,40 +36,52 @@ class Program{
                 Counter2 += Speed;
                 Serial.write( 's' );
                 IncressDifficulty();
-                Counter5++;
+                Counter5++;   //Contador para terminar el juego
                 Down = 0;
                 return( Counter2 );
           }break;       
    }}
    return(Counter2);}
-   int Shoots( int MyTime1, int Speed ){
+   int Shoots( int MyTime1, int Speed ){  //Descripción: Al pasar 2 segundos envía un mensaje por puerto serie. Parámetros: Tiempo transcurrido y dificultad. Retorno: Tiempo actualizado.
       if( MyTime1 == Counter3 ){  //Siempre que pasen 2 seg
         Counter3 += Speed*2;
         Serial.write( 'z' );
       }
       return( Counter3 );
    }
-   int ShipsShoot( int MyTime1, int Speed ){
-      if( MyTime1 == Counter4 ){  //Siempre que pasen 3 seg
+   int ShipsShoot( int MyTime1, int Speed ){  //Descripción: Al pasar 1 segundo envía un mensaje por puerto serie. Parámetros: Tiempo transcurrido y dificultad. Retorno: Tiempo actualizado.
+      if( MyTime1 == Counter4 ){  //Siempre que pasen 1 seg
         Counter4 += Speed;
-        int RandomNumber = int(random( 7 ));
         Serial.write( 'c' );
       }
       return( Counter4 );
    }
-   void IncressDifficulty(){
+   void IncressDifficulty(){  //Descripción: Aumenta la dificultad reduciendo el tiempo de comparación. Parámetros: Ningunos. Retorno: Ninguno.
       Speed -= 8;
    }
-   void GameOver( int Counter5 ){ //Termina el juego tras enviar 8 veces "abajo"
+   void GameOver( int Counter5 ){ //Descripción:Termina el juego tras enviar 10 veces "abajo". Parámetros: Ninguno. Retorno: Ninguno.
       if( Counter5 == 10 ){
         Serial.write( 'G' );
       }
    }
+   /*void SaveScore( char Option, int Time ){
+      switch( Option ){
+        case 'W':{
+            if( (fp=fopen("ScoreGame", "w")) != NULL ){
+              fwrite("SUCCEDED", sizeof(char), 10, fp);}
+          }
+          break;
+        case 'L':{
+            if( (fp=fopen("ScoreGame", "w")) != NULL ){ 
+              fwrite(Time, sizeof(int), 1, fp);}
+          }
+          break;}
+   }*/
 };
 Program Game;
 void setup() {
-   Timer1.initialize();
-   MFS.initialize(&Timer1); // initialize multi-function shield library
+   Timer1.initialize(); // Inicializamos el tiempo interno del shield
+   MFS.initialize(&Timer1); // Inicializamos los pulsadores del shield
    Serial.begin(9600);
 }
 
@@ -81,7 +92,8 @@ void loop() {
   Game.Counter3 = Game.Shoots( Game.Time, Speed );
   Game.Counter4 = Game.ShipsShoot( Game.Time, Speed );
   Game.button = MFS.getButton();
-      
+  Game.Character = Serial.read();
+  //Game.SaveScore( Game.Character, Game.Time ); 
   if ( Game.button ){
     if( Game.button == BUTTON_3_PRESSED ){
       Serial.write( 'R' );} //Envia
