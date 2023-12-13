0399696fcce641bdd4687af551ecedfe00bca6a1
Nieves <143846293+Nieves433@users.noreply.github.com>
2023-12-13 11:24:02 -0300

    Update Arduino Code.ino

--- Código/Arduino/Arduino Code.ino
+++ Código/Arduino/Arduino Code.ino
@@ -1,82 +1,131 @@
 #include <MultiFuncShield.h>
 #define MAX 5
-FILE *fp;
-int Counter1 = 5, Counter5 = 0, Down = 0, MaxRight = 0, Shoot = 1, Speed = 100;
+int CounterUntilWall = 5, Down = 0, MaxRight = 0, Speed = 100;
 class Program{
   public:
-   byte button;
-   char Character;
-   int Counter2 = 1, Counter3 = 2, Counter4 = 2, Time;
-   int MoveAlienShips( int MyTime, int Speed ){ //Descripción: Cada 1 segundo envía un mensaje por puerto serial que indica nueva posición del atacante, sea hacia la izq, der, abajo y si es hacia abajo aumenta la dificultad. Parámetros: Tiempo transcurrido y deficultad. Retorna:Tiempo actualizado.
-    if ( MyTime == Counter2 ){ //MyTime = Counter2 para verificar que paso 1 seg
-        switch( Down ){
-          case 0:{
-            switch( MaxRight ){
-              case 0:{
-                  Counter1--; //cuenta atras hasta chocar contra pared del diaplay
-                  Counter2 += Speed; //Cuenta 1 seg
-                  Serial.write( 'r' );
-                  if ( Counter1 == 0 ){ 
-                    MaxRight = 1; //Máxima posición horizontal alcanzada
-                    Down = 1;}
-                  return( Counter2 );
-              }break;  
-              case 1:{          
-                  Counter1++; //cuenta atras hasta chocar contra esquida del diaplay
-                  Counter2 += Speed; //Cuenta 1 seg  
-                  Serial.write( 'l' );
-                  if ( Counter1 == 5 ){ 
-                    MaxRight = 0;
-                    Down = 1;}
-                  return( Counter2 );
-              }break;
-            }    
-          }break;      
-          case 1:{
-                Counter2 += Speed;
-                Serial.write( 's' );
-                IncressDifficulty();
-                Counter5++;   //Contador para terminar el juego
-                Down = 0;
-                return( Counter2 );
-          }break;       
-   }}
-   return(Counter2);}
-   int Shoots( int MyTime1, int Speed ){  //Descripción: Al pasar 2 segundos envía un mensaje por puerto serie. Parámetros: Tiempo transcurrido y dificultad. Retorno: Tiempo actualizado.
-      if( MyTime1 == Counter3 ){  //Siempre que pasen 2 seg
-        Counter3 += Speed*2;
-        Serial.write( 'z' );
-      }
-      return( Counter3 );
+    char Option;
+    int Pause = 2, CounterTime1 = 1, CounterTime2 = 2, CounterTime3 = 2, CounterGameOver = 0, ArduinoTime;
+    int MoveAlienShips( int ArduinoTime, int Speed ){ //Descripción: Cada 1 segundo envía un mensaje por puerto serial que indica nueva posición del atacante, sea hacia la izq, der, abajo y si es hacia abajo aumenta la dificultad. Parámetros: Tiempo transcurrido y deficultad. Retorna:Tiempo actualizado.
+        if ( ArduinoTime == CounterTime1 ){           //para verificar que paso 1 seg
+            switch( Down ){
+            case 0:{
+                  switch( MaxRight ){
+                  case 0:{
+                         CounterUntilWall--; //cuenta atras hasta chocar contra pared del diaplay
+                         CounterTime1 += Speed; //Cuenta 1 seg
+                         Serial.write( 'r' );
+                         if ( CounterUntilWall == 0 ){ 
+                              MaxRight = 1; //Máxima posición horizontal alcanzada
+                              Down = 1;}
+                  }break;  
+                  case 1:{          
+                         CounterUntilWall++; //cuenta atras hasta chocar contra esquida del diaplay
+                         CounterTime1 += Speed; //Cuenta 1 seg  
+                         Serial.write( 'l' );
+                         if ( CounterUntilWall == 5 ){ 
+                              MaxRight = 0;
+                              Down = 1;}
+                  }break;
+                  }    
+            }break;      
+            case 1:{
+                  CounterTime1 += Speed;
+                  Serial.write( 's' );
+                  IncressDifficulty();
+                  CounterGameOver++;   //Contador para terminar el juego
+                  Down = 0;
+            }break;       
+       }}
+       return(CounterTime1);}
+   int DoBotton(int Pause){
+        byte button;
+        button = MFS.getButton();
+        if ( button ){
+        if( button == BUTTON_3_PRESSED ){
+            Serial.write( 'R' );
+            Sound('r');}
+        if( button == BUTTON_2_PRESSED ){ 
+            Serial.write( 'P');
+            if(Pause == 2){
+                Pause = 1;
+            }else{Pause = 2;}
+            }
+        if( button == BUTTON_1_PRESSED ){
+            Serial.write( 'L' );
+            Sound('l');}
+        }
+        return(Pause);
    }
-   int ShipsShoot( int MyTime1, int Speed ){  //Descripción: Al pasar 1 segundo envía un mensaje por puerto serie. Parámetros: Tiempo transcurrido y dificultad. Retorno: Tiempo actualizado.
-      if( MyTime1 == Counter4 ){  //Siempre que pasen 1 seg
-        Counter4 += Speed;
-        Serial.write( 'c' );
-      }
-      return( Counter4 );
+   int GunnerShots( int ArduinoTime, int Speed ){  //Descripción: Al pasar 2 segundos envía un mensaje por puerto serie. Parámetros: Tiempo transcurrido y dificultad. Retorno: Tiempo actualizado.
+        if( ArduinoTime == CounterTime2 ){  //Siempre que pasen 2 seg
+          CounterTime2 += Speed*2;
+          Serial.write( 'z' );
+        }
+        return( CounterTime2 );
+   }
+   int ShipsShot( int ArduinoTime, int Speed ){  //Descripción: Al pasar 1 segundo envía un mensaje por puerto serie. Parámetros: Tiempo transcurrido y dificultad. Retorno: Tiempo actualizado.
+        if( ArduinoTime == CounterTime3 ){  //Siempre que pasen 1 seg
+          CounterTime3 += Speed;
+          Serial.write( 'c' );
+        }
+        return( CounterTime3 );
    }
    void IncressDifficulty(){  //Descripción: Aumenta la dificultad reduciendo el tiempo de comparación. Parámetros: Ningunos. Retorno: Ninguno.
-      Speed -= 8;
+        Speed -= 8;
    }
-   void GameOver( int Counter5 ){ //Descripción:Termina el juego tras enviar 10 veces "abajo". Parámetros: Ninguno. Retorno: Ninguno.
-      if( Counter5 == 10 ){
-        Serial.write( 'G' );
-      }
+   void GameOver( int CounterGameOver ){ //Descripción:Termina el juego tras enviar 10 veces "abajo". Parámetros: Ninguno. Retorno: Ninguno.
+        if( CounterGameOver == 10 ){
+          Serial.write( 'G' );
+          Sound('L');
+        }
    }
-   /*void SaveScore( char Option, int Time ){
-      switch( Option ){
-        case 'W':{
-            if( (fp=fopen("ScoreGame", "w")) != NULL ){
-              fwrite("SUCCEDED", sizeof(char), 10, fp);}
+   int onPause1(int ArduinoTime){
+        if ( ArduinoTime == CounterTime1 ){ //MyTime = Counter2 para verificar que paso 1 seg
+            CounterTime1 += Speed; //Cuenta 1 seg
+        }
+        return( CounterTime1 );
+   }     
+   int onPause2(int ArduinoTime){
+        if( ArduinoTime == CounterTime2 ){  //Siempre que pasen 1 seg
+            CounterTime2 += Speed; //Cuenta 1 seg
+        }
+        return( CounterTime2 );
+   }
+   int onPause3(int ArduinoTime){
+        if( ArduinoTime == CounterTime3 ){  //Siempre que pasen 2 seg
+            CounterTime3 += Speed*2; //Cuenta 2 seg
+        }
+        return( CounterTime3 );
+   }
+   void Sound(char Option){
+        switch(Option){
+            case 'r':{
+              MFS.beep(5);}
+            break;
+            case 'l':{
+              MFS.beep(5);}
+            break;
+            case 'W':{
+               delay(1000); //Retraso de música
+               for(int i=0; i<2; i++){
+                   MFS.beep(7); delay(300);
+                   MFS.beep(7); delay(300);
+                   MFS.beep(15); delay(600);}
+                   MFS.beep(4); delay(250);
+                   MFS.beep(4); delay(250);
+                   MFS.beep(10); delay(600);
+                   MFS.beep(4); delay(150);
+                   MFS.beep(20);}
+            break;
+            case 'L':{
+                 delay(1000); //Retraso de música
+                 MFS.beep(5,20,3,1); delay(1000);
+                 MFS.beep(10,10,3,1); delay(1200);
+                 MFS.beep(20); delay(500);
+                 MFS.beep(10);}
+            break;
           }
-          break;
-        case 'L':{
-            if( (fp=fopen("ScoreGame", "w")) != NULL ){ 
-              fwrite(Time, sizeof(int), 1, fp);}
-          }
-          break;}
-   }*/
+   }
 };
 Program Game;
 void setup() {
@@ -86,18 +135,23 @@ void setup() {
 }
 
 void loop() {
-  Game.GameOver( Counter5 );
-  Game.Time = millis()/10;
-  Game.Counter2 = Game.MoveAlienShips( Game.Time, Speed );
-  Game.Counter3 = Game.Shoots( Game.Time, Speed );
-  Game.Counter4 = Game.ShipsShoot( Game.Time, Speed );
-  Game.button = MFS.getButton();
-  Game.Character = Serial.read();
-  //Game.SaveScore( Game.Character, Game.Time ); 
-  if ( Game.button ){
-    if( Game.button == BUTTON_3_PRESSED ){
-      Serial.write( 'R' );} //Envia
-    if( Game.button == BUTTON_1_PRESSED ){
-      Serial.write( 'L' );} //Envia
-  }
+    switch( Game.Pause ){
+           case 1:{
+                Game.ArduinoTime = millis()/10;
+                Game.Pause = Game.DoBotton(Game.Pause);
+                Game.CounterTime1 = Game.onPause1(Game.ArduinoTime);
+                Game.CounterTime2 = Game.onPause3(Game.ArduinoTime);
+                Game.CounterTime3 = Game.onPause2(Game.ArduinoTime);}
+           break;
+           case 2:{
+                Game.ArduinoTime = millis()/10;
+                Game.Option = Serial.read();
+                Game.GameOver( Game.CounterGameOver );
+                Game.CounterTime1 = Game.MoveAlienShips( Game.ArduinoTime, Speed );
+                Game.CounterTime2 = Game.GunnerShots( Game.ArduinoTime, Speed );
+                Game.CounterTime3 = Game.ShipsShot( Game.ArduinoTime, Speed );
+                Game.Pause = Game.DoBotton(Game.Pause);
+                Game.Sound(Game.Option);}
+           break;
+    }
 }
